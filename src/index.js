8a40eaa063970aa9cb37871ec70b4d742817c539
Muhammadfraz98 <muhammadfraz98@gmail.com>
2021-07-15 12:51:20 +0500

    third commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Navbar from './components/AppBar/appBar';
 import Footer from './components/Footer/footer';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -17,4 +17,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
