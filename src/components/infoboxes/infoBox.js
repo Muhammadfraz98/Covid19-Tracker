4fc04bbf6eeb7a53ed763ff4271881454e9cb382
Muhammadfraz98 <muhammadfraz98@gmail.com>
2021-07-16 12:11:51 +0500

    commit

--- src/components/infoboxes/infoBox.js
+++ src/components/infoboxes/infoBox.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './infoBox.css'
-import { Card, CardContent, Typography } from "@material-ui/core"
+import { Card, CardActionArea, CardContent, Typography } from "@material-ui/core"
 
 const InfoBox = ({ title, cases, total, active, isRed, isBlue, ...props }) => {
+
+    const [shadow, setShadow] = useState(1)
+    const onMouseOver = () => setShadow({ shadow: 3 });
+    const onMouseOut = () => setShadow({ shadow: 1 });
+
     return (
-        <Card onClick={props.onClick} className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"} ${isBlue && "infoBox--blue"}`}>
-            <CardContent>
-                <Typography className="infoBox__title" color="textSecondary">
-                    {/* Title */}
-                    {title}
-                </Typography>
+        <Card onClick={props.onClick} className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"} ${isBlue && "infoBox--blue"}`}
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}
+        >
+            <CardActionArea>
+                <CardContent>
+                    <Typography className="infoBox__title" color="textSecondary">
+                        {/* Title */}
+                        {title}
+                    </Typography>
 
-                {/* number of cases */}
-                <h2 className={`infoBox__cases ${!isRed && !isBlue && "infoBox__cases--green"}`}>  {cases} </h2>
+                    {/* number of cases */}
+                    <h2 className={`infoBox__cases ${!isRed && !isBlue && "infoBox__cases--green"}`}>  {cases} </h2>
 
-                <Typography className="infoBox__total" color="textSecondary">
-                    {/* total */}
-                    {total} Total
-                </Typography>
-            </CardContent>
+                    <Typography className="infoBox__total" color="textSecondary">
+                        {/* total */}
+                        {total} Total
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+
         </Card>
 
     )
